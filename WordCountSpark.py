0dd49289ae85a8d015508d6543716a35069f6a4c
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-10 15:28:51 -0500

    fix

--- WordCountSpark.py
+++ WordCountSpark.py
@@ -4,14 +4,12 @@ Created on Jun 10, 2017
 @author: SathishParthasarathy
 '''
 from pyspark import SparkConf, SparkContext
-from hdfs3 import HDFileSystem
+
 if __name__ == '__main__':
     conf = SparkConf().setAppName("Word Count - Python")
     spark = SparkContext(conf = conf)
-    hdfs = HDFileSystem('hadoop.master.com', port=9000)
-    if hdfs.exists("/user/psathishcs/Input/Books/The_Outline_of_Science.txt") != True:
-        text_file = spark.textFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/The_Outline_of_Science.txt")
-        words = text_file.flatMap(lambda line: line.split())
-        wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b:  a + b)
-        for line in wordCounts.take(1000):
-            print line        
+    text_file = spark.textFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/The_Outline_of_Science.txt")
+    words = text_file.flatMap(lambda line: line.split())
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b:  a + b)
+    for line in wordCounts.take(1000):
+        print line        
