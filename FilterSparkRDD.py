4ff57cf635ebf7a8bb0e25f974b76d2d57e85083
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-11 08:24:39 -0500

    fix

--- FilterSparkRDD.py
+++ FilterSparkRDD.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     spark = SparkContext(conf = conf)
     text_file = spark.textFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/The_Outline_of_Science.txt")
     print "No of Lines -> %d" % (text_file.count())
-    print "First -> %d" % (text_file.first())
+    print "First -> %s" % (text_file.first())
     sciLines =  text_file.filter(lambda line: "Science" in line)
     sciLines.saveAsTextFile("hdfs://hadoop.master.com:9000/user/psathishcs/Output/Books/ScienceSci_Python");
     sciWordLines = text_file.filter( lambda line : hasScience(line))
